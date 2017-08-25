154c51845ed5557808f79f71cd3c895588f94f3b
Feng, Xiao-hui <546915457@qq.com>
2017-08-25 16:57:37 +0800

    init

--- src/android/InAppWebView.java
+++ src/android/InAppWebView.java
@@ -23,7 +23,7 @@ public class InAppWebView extends CordovaPlugin {
     private static final String LOG_TAG = "InAppWebView";
 
     private String errorFile = null;
-    private String failingUrl;
+    private static String  failingUrl;
 
     private String jsFile = null;
     private String jsLoader = null;
